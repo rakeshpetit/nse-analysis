5fa011a3fa7cb74b0046b46f50cd5fd1ed9a67c1
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-04-26 16:22:10 +0530

    Don't download duplicates

--- downloadUrl.js
+++ downloadUrl.js
@@ -2,6 +2,9 @@ import fs from 'fs'
 import https from 'https'
 
 const downloadUrl = (fileUrl, fileName, filePath) => {
+    if (fs.existsSync(fileName)) {
+        return
+    }
     console.log(fileName)
     const file = fs.createWriteStream(fileName);
     https.get(fileUrl, function (response) {
