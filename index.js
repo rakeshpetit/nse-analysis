42e11bca4da733cd4360c7466ef09bc4007545f1
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-04-29 11:02:11 +0530

    find daily percentage differences

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import { getURLs } from './getDates'
 import { downloadAsync, unZipAsync } from './downloadAsync'
 import { collateData } from './collateData'
@@ -12,7 +13,36 @@ const urls = getURLs(startingDateStr, endingDateStr)
 // unZipAsync(urls)
 // collateData(urls)
 cleanData(startingDateStr, endingDateStr, (cleanedData) => {
-    console.log(cleanedData)
+    // console.log(cleanedData)
+    const startingDate = moment(startingDateStr, 'DD/MM/YYYY HH:mm')
+    const endingDate = moment(endingDateStr, 'DD/MM/YYYY HH:mm')
+    const currentDate = startingDate.clone()
+    const numOfDays = endingDate.diff(startingDate, "days")
+    let diffData = {}
+    Object.keys(cleanedData).map(ticker => {
+        const tickerData = Object.keys(cleanedData[ticker])
+        tickerData.sort((a,b) => {
+            const dateA = moment(a, 'DD/MM/YYYY')
+            const dateB = moment(b, 'DD/MM/YYYY')
+            return dateA.isSameOrAfter(dateB) ? 1 : -1
+        })
+        const diffPercentData = []
+        for(let i=0; i<tickerData.length; i++) {
+            const currentDay = tickerData[i]
+            const previousDay = i > 0 ? tickerData[i-1] : currentDay
+            const currentDayData = cleanedData[ticker][currentDay].close
+            const previousDayData = cleanedData[ticker][previousDay].close
+            const percentDiff = 100 * (currentDayData - previousDayData) / previousDayData
+            diffPercentData.push({
+                currentDate: currentDay,
+                percentDiff: percentDiff
+            })
+        }
+        diffData[ticker] = {
+            percentData: diffPercentData
+        }
+    })
+    console.log(diffData['ZYDUSWELL'])
 })
 
 
