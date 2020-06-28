d355f62c7d763222ab12c7f02965c0fe21d93829
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-06-28 23:00:57 +0530

    code cleanup

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ import { getURLs } from './getDates'
 import { downloadAsync, unZipAsync } from './downloadAsync'
 import { collateData } from './collateData'
 import { counter } from './reducers'
-// import { calculateMarks, analyseData, filterData } from './analyseData'
 import { getCleanData, analyseData, getMonthlyList } from './analyseDataNew'
-// import { writeToCsv } from './dataToCsv'
+import { writeToCsv } from './dataToCsv'
 const startingDateStr = '01/01/2018'
 const endingDateStr = '31/12/2018'
 
@@ -18,12 +17,6 @@ const urls = getURLs(startingDateStr, endingDateStr)
 // writeToCsv(startingDateStr, endingDateStr)
 
 export const store = createStore(counter)
-
-// store.dispatch({ type: 'INCREMENT' })
-// store.dispatch({ type: 'INCREMENT' })
-// console.log(store.getState())
-// store.dispatch({ type: 'DECREMENT' })
-// store.dispatch({ type: 'DECREMENT' })
 
 getCleanData('01/01/2019').then(() => {
     const yearly = analyseData("01/01/2018", "01/01/2019")
