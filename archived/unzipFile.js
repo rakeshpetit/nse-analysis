1b70c6bd3cddb6f3cd1d74caf8476453fb7da99f
Rakesh Arunachalam <rakesh@thoughtbot.com>
2025-02-14 20:43:32 +0000

    Archive current code and rewrite the project

---
+++ archived/unzipFile.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import StreamZip from "node-stream-zip";
+import { getFileWithPath, getFilePath } from "./downloadUrl";
+
+const unZip = (url, fileName, filePath) => {
+  try {
+    const directoryPath = getFilePath(filePath, "csvs");
+    fs.mkdirSync(directoryPath, { recursive: true });
+    const fileWithPath = getFileWithPath(fileName, filePath, "zips");
+    const zip = new StreamZip({
+      file: fileWithPath,
+      storeEntries: true,
+    });
+    zip.on("error", () => {
+      return true;
+    });
+    zip.on("ready", () => {
+      zip.extract(null, directoryPath, (err, count) => {
+        zip.close();
+      });
+    });
+  } catch (e) {
+    console.log("Error at", url);
+    console.log(e);
+  }
+};
+
+export { unZip };
